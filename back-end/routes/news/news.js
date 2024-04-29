1c745b7236910b826fc370d5cc7a43fe44f7b8ee
sdickson603 <you@example.com>
2024-04-29 16:05:08 -0400

    news POST endpoint working

--- back-end/routes/news/news.js
+++ back-end/routes/news/news.js
@@ -38,7 +38,7 @@ router.get('/', async (req, res) => {
     let fixedQueryObject = {
         "country": "us",
         "q": "news"
-    }
+    };
     let queryObject = addApiKey(fixedQueryObject);
     let url = createUrlFromQueryObject(queryObject);
     let newsArticles = await fetchData(url);
@@ -46,7 +46,11 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    res.send("POST called");  
+    let query = req.body;
+    let queryObjectWithApiKey = addApiKey(query);
+    let url = createUrlFromQueryObject(queryObjectWithApiKey);
+    let newsArticles = await fetchData(url);
+    res.send(newsArticles);  
 });
 
 export default router;
