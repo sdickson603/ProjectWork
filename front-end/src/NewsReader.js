3dfb309bab981e4cd679687b34dc8bc156005f6d
sdickson603 <you@example.com>
2024-04-30 13:28:15 -0400

    now gets live article list from server

--- front-end/src/NewsReader.js
+++ front-end/src/NewsReader.js
@@ -1,4 +1,3 @@
-
 import { QueryForm } from './QueryForm';
 import { Articles } from './Articles';
 import { useState, useEffect } from 'react';
@@ -8,6 +7,7 @@ export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
   const [data, setData] = useState(exampleData);   // current data returned from newsapi
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
+  const urlNews="/news"
 
   useEffect(() => {
     getNews(query);
@@ -19,7 +19,21 @@ export function NewsReader() {
 
   async function getNews(queryObject) {
     if (queryObject.q) {
-        setData(exampleData);
+      try {
+        const response = await fetch(urlNews, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(queryObject),
+        });
+  
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
     } else {
       setData({});
     }
