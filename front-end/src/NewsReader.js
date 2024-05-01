31beb6082fb1c0eb66e63cdaeda06cacecf13717
go-andy-go <andy.m.bryant@gmail.com>
2024-05-01 08:28:11 -0400

    Login capability is now available

--- front-end/src/NewsReader.js
+++ front-end/src/NewsReader.js
@@ -1,44 +1,78 @@
 import { QueryForm } from './QueryForm';
 import { Articles } from './Articles';
 import { useState, useEffect } from 'react';
-import { exampleQuery ,exampleData } from './data';
+import { exampleQuery, exampleData } from './data';
 import { SavedQueries } from './SavedQueries';
-
+import { LoginForm } from './LoginForm';
 
 export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
   const [data, setData] = useState(exampleData);   // current data returned from newsapi
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
-  const urlNews="/news";
-  const urlQueries = "/queries"
   const [savedQueries, setSavedQueries] = useState([{ ...exampleQuery }]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [credentials, setCredentials] = useState({ user: "", password: "" });
+  const urlNews = "/news"
+  const urlQueries = "/queries"
+  const urlUsersAuth = "/users/authenticate";
+
+
+
 
   useEffect(() => {
     getNews(query);
   }, [query])
 
-  useEffect(() => {getQueryList();}, [])
+  useEffect(() => { getQueryList(); }, [])
 
   async function getQueryList() {
     try {
       const response = await fetch(urlQueries);
-        if (response.ok) {
+      if (response.ok) {
         const data = await response.json();
-      console.log("savedQueries has been retrieved: ");
-      setSavedQueries(data);
-    }
+        console.log("savedQueries has been retrieved: ");
+        setSavedQueries(data);
+      }
     } catch (error) {
-    console.error('Error fetching news:', error);
+      console.error('Error fetching news:', error);
     }
   }
-    
+
+
+  async function login() {
+    if (currentUser !== null) {
+      // logout
+      setCurrentUser(null);
+    } else {
+      // login
+      try {
+        const response = await fetch(urlUsersAuth, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(credentials),
+        });
+        if (response.status === 200) {
+          setCurrentUser({ ...credentials });
+          setCredentials({ user: "", password: "" });
+        } else {
+          alert("Error during authentication! " + credentials.user + "/" + credentials.password);
+          setCurrentUser(null);
+        }
+      } catch (error) {
+        console.error('Error authenticating user:', error);
+        setCurrentUser(null);
+      }
+    }
+  }
+
+
   async function saveQueryList(savedQueries) {
     try {
       const response = await fetch(urlQueries, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(savedQueries),
-    });
+      });
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -50,24 +84,24 @@ export function NewsReader() {
   }
 
 
-function onSavedQuerySelect(selectedQuery) {
+  function onSavedQuerySelect(selectedQuery) {
     setQueryFormObject(selectedQuery);
     setQuery(selectedQuery);
-}
+  }
 
-function onFormSubmit(queryObject) {
+  function onFormSubmit(queryObject) {
     let newSavedQueries = [];
     newSavedQueries.push(queryObject);
     for (let query of savedQueries) {
-    if (query.queryName !== queryObject.queryName) {
-      newSavedQueries.push(query);
+      if (query.queryName !== queryObject.queryName) {
+        newSavedQueries.push(query);
+      }
     }
+    console.log(JSON.stringify(newSavedQueries));
+    saveQueryList(newSavedQueries);
+    setSavedQueries(newSavedQueries);
+    setQuery(queryObject);
   }
-  console.log(JSON.stringify(newSavedQueries));
-  saveQueryList(newSavedQueries);
-  setSavedQueries(newSavedQueries);
-  setQuery(queryObject);
-}
 
   async function getNews(queryObject) {
     if (queryObject.q) {
@@ -77,7 +111,7 @@ function onFormSubmit(queryObject) {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(queryObject),
         });
-  
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -93,6 +127,10 @@ function onFormSubmit(queryObject) {
 
   return (
     <div>
+      <LoginForm login={login}
+        credentials={credentials}
+        currentUser={currentUser}
+        setCredentials={setCredentials} />
       <div >
         <section className="parent" >
           <div className="box">
@@ -105,7 +143,7 @@ function onFormSubmit(queryObject) {
           <div className="box">
             <span className='title'>Saved Queries</span>
             <SavedQueries savedQueries={savedQueries}
-              selectedQueryName={query.queryName} 
+              selectedQueryName={query.queryName}
               onQuerySelect={onSavedQuerySelect} />
           </div>
           <div className="box">
