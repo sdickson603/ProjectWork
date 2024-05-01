ed4cfa5872415aa577296ce3516af3202132bcb4
sdickson603 <you@example.com>
2024-05-01 12:17:28 -0400

    Features 1 and 2 and some styling

--- front-end/src/Articles.js
+++ front-end/src/Articles.js
@@ -1,11 +1,15 @@
 export function Articles(params) {
     let articles = (params.data.articles)?params.data.articles:[];
     let queryName = (params.query.queryName)?params.query.queryName:"na";
+    let queryText = (params.query.q)?params.query.q:"";
     let articleCount = (params.data.totalResults)?params.data.totalResults:0;
     return (
       <div>
         Query: {queryName}
-        <br/>Count: {articleCount}
+        <br/>
+        Query Text: {queryText}
+        <br/>
+        Count: {articleCount}
         <ol >{
             articles.map((item, idx) => {
               if(item){
@@ -13,8 +17,13 @@ export function Articles(params) {
                   if(item.title === "[Removed]"){
                     return (<li key={idx} >Was Removed</li>);
                   }
-                  let trimTitle = item.title.substring(0,30);
-                  return (<li key={idx}>{trimTitle}<a href={item.url} target="_blank" rel="noreferrer" >&nbsp;Link</a></li>);    
+                  let trimTitle = item.title.substring(0,85);
+                  return (
+                  <li key={idx}>
+                    <a href={item.url} target="_blank" rel="noreferrer" >
+                    {trimTitle}
+                    </a>
+                  </li>);    
                 }else{
                   return (<li key={idx}>No Title</li>);
                 }
